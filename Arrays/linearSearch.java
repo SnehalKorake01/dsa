5bd350459425a046ab92e6f4aee2033f6637282d
snehal <snehalkorake5@gmail.com>
2026-05-19 22:37:36 +0530

    added

---
+++ Arrays/linearSearch.java
@@ -0,0 +1,12 @@
+class Demo{
+	public static void main(String[]args){
+		int[]arr={10,20,30,40,50};
+		int target=30;
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]==target){
+				System.out.print(i);
+				break;
+			}
+		}
+	}
+}
