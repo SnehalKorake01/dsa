61e10c24d6025e4925acbaad967c5a9fa8f59e07
snehal <snehalkorake5@gmail.com>
2026-05-29 00:21:01 +0530

    added

---
+++ DigitExtraction/count.java
@@ -0,0 +1,11 @@
+class Demo{
+	public static void main(String[]args){
+		int n=123456;
+		int count=0;
+		while(n!=0){
+			count++;
+			n=n/10;
+		}
+		System.out.println(count);
+	}
+}
---
+++ DigitExtraction/sum.java
@@ -0,0 +1,12 @@
+class Demo{
+	public static void main(String[]args){
+		int n=12345;
+		int sum=0;
+		while(n!=0){
+			int rem=n%10;
+			sum +=rem;
+			n=n/10;
+		}
+		System.out.println(sum);
+	}
+}
