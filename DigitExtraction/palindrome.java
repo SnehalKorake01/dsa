5ae2c7ffeba1e4ef942749c936d5ff0ca252cc14
snehal <snehalkorake5@gmail.com>
2026-05-28 23:58:27 +0530

    added

---
+++ DigitExtraction/palindrome.java
@@ -0,0 +1,18 @@
+class Demo{
+	public static void main(String[]args){
+		int n=123;
+		int store=n;
+		int rev=0;
+		while(n!=0){
+			int rem=n%10;
+			rev=rev*10+rem;
+			n=n/10;
+		}
+		if(rev==store){
+			System.out.println("palindrome no");
+		}
+		else{
+			System.out.println("not palindrome");
+		}
+	}
+}
